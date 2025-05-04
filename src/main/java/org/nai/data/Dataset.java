0af4608f6160c80435bb2d2f34aff997c9c233c8
druidmentalized <dmitry.barmuta@gmail.com>
2025-05-04 19:26:37 +0200

    Introduced Dataset class

--- src/main/java/org/nai/data/Dataset.java
+++ src/main/java/org/nai/data/Dataset.java
@@ -1,5 +1,31 @@
 package org.nai.data;
 
-public class Dataset {
+import org.nai.structures.Pair;
+import org.nai.structures.Vector;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dataset {
+    private final List<Pair<Integer, Vector>> data;
+
+    public Dataset() {
+        this.data = new ArrayList<>();
+    }
+
+    public Dataset(List<Pair<Integer, Vector>> data) {
+        this.data = data;
+    }
+
+    public List<Pair<Integer, Vector>> getData() {
+        return data;
+    }
+
+    public void add(Pair<Integer, Vector> pair) {
+        data.add(pair);
+    }
+
+    public int size() {
+        return data.size();
+    }
 }
