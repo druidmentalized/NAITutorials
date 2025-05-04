673153266cba86309ad5323ba1add4bb465819e1
druidmentalized <dmitry.barmuta@gmail.com>
2025-05-04 19:18:29 +0200

    Added new Vector class, which will be used in all later classes after migration to it

---
+++ src/main/java/org/nai/data/Dataset.java
@@ -0,0 +1,5 @@
+package org.nai.data;
+
+public class Dataset {
+
+}
---
+++ src/main/java/org/nai/structures/Vector.java
@@ -0,0 +1,74 @@
+package org.nai.structures;
+
+import java.util.Arrays;
+
+public class Vector {
+    private final double[] data;
+
+    public Vector(double[] data) {
+        this.data = data.clone();
+    }
+
+    public int size() {
+        return data.length;
+    }
+
+    public double[] getData() {
+        return data.clone();
+    }
+
+    public Vector add(Vector other) {
+        checkDimension(other);
+        double[] result = new double[data.length];
+        for (int i = 0; i < data.length; i++) {
+            result[i] = data[i] + other.data[i];
+        }
+        return new Vector(result);
+    }
+
+    public Vector scale(double scalar) {
+        double[] result = new double[data.length];
+        for (int i = 0; i < data.length; i++) {
+            result[i] = data[i] * scalar;
+        }
+        return new Vector(result);
+    }
+
+    public double dot(Vector other) {
+        checkDimension(other);
+        double result = 0;
+        for (int i = 0; i < data.length; i++) {
+            result += data[i] * other.data[i];
+        }
+        return result;
+    }
+
+    public double norm() {
+        return Math.sqrt(dot(this));
+    }
+
+    public double distanceTo(Vector other) {
+        return Math.sqrt(squaredDistanceTo(other));
+    }
+
+    public double squaredDistanceTo(Vector other) {
+        checkDimension(other);
+        double sum = 0;
+        for (int i = 0; i < data.length; i++) {
+            double d = data[i] - other.data[i];
+            sum += d * d;
+        }
+        return sum;
+    }
+
+    private void checkDimension(Vector other) {
+        if (data.length != other.data.length) {
+            throw new IllegalArgumentException("Vectors must be the same length");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(data);
+    }
+}
